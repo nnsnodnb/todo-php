4ac07669ce7e56cc57acf4f6e4d795e40260e767
nnsnodnb <ahr63_gej@me.com>
2015-09-08 03:53:34 +0900

    Synchronization with config.php

--- config.sample.php
+++ config.sample.php
@@ -1,13 +1,16 @@
 <?php
 
-define('dns' , 'mysql:host=localhost;dbname=todo-php') ;
-define('user' , 'root') ;
-define('password' , 'root') ;
+$dsn = 'mysql:host=localhost;dbname=todo_php';
+$user = 'root';
+$password = 'password';
 
-define('SITE_URL' , 'http://localhost/php/todo-php') ;
+$dbh = new PDO($dsn, $user, $password);
+$dbh->query('SET NAMES UTF8');
+
+define('SITE_URL' , 'http://localhost/todo-php') ;
 
 error_reporting(E_ALL & ~E_NOTICE) ;
 
-session_set_cookie_params(0 , 'php/todo-php') ;
+session_set_cookie_params(0 , 'todo-php') ;
 
 ?>
