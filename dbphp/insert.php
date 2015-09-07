16e9c4eef1ddcfe99ca2fcb931309dd41e1fbb23
nnsnodnb <ahr63_gej@me.com>
2015-09-08 03:55:14 +0900

    Create file for insert SQL

--- dbphp/insert.php
+++ dbphp/insert.php
@@ -2,6 +2,22 @@
 
 require_once('../config.php');
 
-header("Location: "SITE_URL"");
+$title = $_POST['title'];
+$scheduled = $_POST['scheduled'];
+$rank = $_POST['rank'];
+$chk = 0;
+
+$sql = $dbh->prepare("INSERT INTO task (TITLE , SCHEDULE_DATE , RANK , FINISH_CHECK) VALUES (:title , :scheduled , :rank , :finish_chk)");
+
+$sql->bindValue(':title' , $title , PDO::PARAM_STR);
+$sql->bindValue(':scheduled' , $scheduled , PDO::PARAM_STR);
+$sql->bindValue(':rank' , $rank , PDO::PARAM_STR);
+$sql->bindValue(':finish_chk' , (int) $chk , PDO::PARAM_INT);
+
+$sql->execute();
+
+$dbh = null;
+
+header("Location: ".SITE_URL."");
 
 ?>
